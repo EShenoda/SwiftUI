f1c28ca6aea182d70d4da0bed38f7431588a0f92
Emil Shenoda <ESShenoda@student.fullsail.edu>
2023-09-27 17:11:28 -0400

    ADD- NavigationView, ScrollView

--- Apple_FrameworksV2/Apple_FrameworksV2/Framework.swift
+++ Apple_FrameworksV2/Apple_FrameworksV2/Framework.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-
-struct Framework {
-    
-    // let id = UUID()
+// Hashable: unique identifier
+// Identifiable: needs a UUID
+struct Framework: Hashable, Identifiable {
+    let id = UUID()
     let name: String
     let imageName: String
     let urlString: String
--- Apple_FrameworksV2/Apple_FrameworksV2/FrameworkGridView.swift
+++ Apple_FrameworksV2/Apple_FrameworksV2/FrameworkGridView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columnsArray: [GridItem] = [GridItem(.flexible()),
+                                    GridItem(.flexible()),
+                                    GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView{
+            
+            ScrollView{
+                
+                LazyVGrid(columns: columnsArray){
+                    
+                    ForEach(MockData.frameworksArray){ frameworkObject in
+                        FrameworkTitleView(frameworkToDisplay: frameworkObject)
+                    }
+                }
+                .navigationTitle("🍎 Frameworks")
+            }
+        }
     }
 }
+
+struct FrameworkTitleView: View {
+  
+    let frameworkToDisplay: Framework
+    
+    var body: some View {
+        
+        VStack {
+            
+            Image(frameworkToDisplay.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(frameworkToDisplay.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
     }
 }
