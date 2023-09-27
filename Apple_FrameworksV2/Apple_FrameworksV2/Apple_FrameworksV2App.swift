88efc2dadd4e14ccc99b87e3925974e09f0badc3
Emil Shenoda <ESShenoda@student.fullsail.edu>
2023-09-27 15:50:27 -0400

    ADD- Frameworks App Version 2

---
+++ Apple_FrameworksV2/Apple_FrameworksV2/Apple_FrameworksV2App.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksV2App.swift
+//  Apple_FrameworksV2
+//
+//  Created by emil shenoda on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_FrameworksV2/Apple_FrameworksV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple_FrameworksV2
+//
+//  Created by emil shenoda on 9/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
