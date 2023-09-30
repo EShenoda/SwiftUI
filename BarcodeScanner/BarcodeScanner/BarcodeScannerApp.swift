941d1a72cd5016e3efe81eb4e7004de2b3079f5c
Emil Shenoda <ESShenoda@student.fullsail.edu>
2023-09-30 13:22:25 -0400

    ADD- BarcodeScanner project

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by emil shenoda on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
